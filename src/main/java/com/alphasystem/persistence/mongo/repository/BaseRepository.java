6facfee8e37e5dbe23d5bca76babbfb7b6acdae0
sali <sali2@loyalty.com>
2015-12-18 18:56:34 -0500

    Using Spring Annotation processor

--- src/main/java/com/alphasystem/persistence/mongo/repository/BaseRepository.java
+++ src/main/java/com/alphasystem/persistence/mongo/repository/BaseRepository.java
@@ -1,25 +1,25 @@
 /**
- * 
+ *
  */
 package com.alphasystem.persistence.mongo.repository;
 
 import com.alphasystem.persistence.model.AbstractDocument;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 
 /**
  * @author sali
- * 
  */
 @NoRepositoryBean
-public interface BaseRepository<T extends AbstractDocument> extends
-		PagingAndSortingRepository<T, String> {
+public interface BaseRepository<T extends AbstractDocument> extends PagingAndSortingRepository<T, String>,
+        QueryDslPredicateExecutor<T> {
 
-	/**
-	 * @param displayName
-	 * @return
-	 */
-	public T findByDisplayName(String displayName);
+    /**
+     * @param displayName
+     * @return
+     */
+    public T findByDisplayName(String displayName);
 
 }
